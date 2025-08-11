6f48becd5a0e76e6c49beac5a88df92d569f030a
David Wuibaille <david.wuibaille@outlook.fr>
2025-08-11 16:17:07 +0200

    Auto-commit: 2025-08-11 16:17:07

--- UninstallKB/uninstallKB.cmd
+++
@@ -1,8 +0,0 @@
-xcopy "%~dp0PSWindowsUpdate" "C:\Program Files\WindowsPowerShell\Modules\PSWindowsUpdate\" /S /Y /E
-
-Set cmdpowershell=powershell
-if defined PROCESSOR_ARCHITEW6432 Set cmdpowershell=%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe
-
- 
-%cmdpowershell% -noprofile -command "Set-ExecutionPolicy bypass LocalMachine"
-%cmdpowershell% -file "%~dp0uninstallKB.ps1"
